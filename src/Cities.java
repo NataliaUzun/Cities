8e6a380df41a605d29c9038bd73a43bbd9d9e4ec
nataliauzun <=>
2023-04-06 15:53:30 +0200

    Revert "Madrid addad"
    This reverts commit de6d1dd785ffb36743526beece1a4b36c948a7f2.

--- src/Cities.java
+++ src/Cities.java
@@ -1,8 +1,5 @@
-import java.sql.SQLOutput;
-
 public class Cities {
     public void toVisit(){
         System.out.println("to visit Bremen");
-        System.out.println("to visit Madrid");
     }
 }
